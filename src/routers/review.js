a002f429cd2042df30fe8f80b417b78fc16c33e7
Erica-Brock <Eady.Brock@gmail.com>
2017-12-07 18:10:04 -0600

    updated review and user routes

--- src/routers/review.js
+++ src/routers/review.js
@@ -4,10 +4,10 @@ const express_1 = require("express");
 const review_ctrl_1 = require("../controllers/review.ctrl");
 const router = express_1.Router();
 router
-    .get('/', review_ctrl_1.read)
-    .get('/:id', review_ctrl_1.all)
+    .get('/', review_ctrl_1.all)
+    .get('/:id', review_ctrl_1.read)
     .get('/poster/:id', review_ctrl_1.allPoster)
-    .get('/reciever/:id', review_ctrl_1.allReceiver)
+    .get('/receiver/:id', review_ctrl_1.allReceiver)
     .post('/', review_ctrl_1.create)
     .put('/', review_ctrl_1.update)
     .delete('/', review_ctrl_1.destroy);
