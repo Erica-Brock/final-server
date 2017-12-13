864b08d9b21b89fb8042fe1f80a3c60a89d4b424
Erica-Brock <Eady.Brock@gmail.com>
2017-12-13 10:23:23 -0600

    merger

--- src/controllers/review.ctrl.js
+++ src/controllers/review.ctrl.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const review_proc_1 = require("../procedures/review.proc");
 exports.create = (req, res, next) => {
-    review_proc_1.default.create(req.body.poster, req.body.receiver, req.body.review)
-        .then((sets) => {
-        res.json(sets);
+    review_proc_1.default.create(req.body.clientId, req.body.providerId, +req.body.rating, req.body.review)
+        .then((set) => {
+        res.json(set);
     });
 };
 exports.read = (req, res, next) => {
@@ -31,14 +31,20 @@ exports.all = (req, res, next) => {
         res.json(sets);
     });
 };
-exports.allPoster = (req, res, next) => {
-    review_proc_1.default.allPoster(+req.params.id)
+exports.allByProvider = (req, res, next) => {
+    review_proc_1.default.allByProvider(+req.params.id)
         .then((sets) => {
         res.json(sets);
     });
 };
 exports.allReceiver = (req, res, next) => {
     review_proc_1.default.allReceiver(+req.params.id)
+        .then((sets) => {
+        res.json(sets);
+    });
+};
+exports.allByJob = (req, res, next) => {
+    review_proc_1.default.allByJob(+req.params.id)
         .then((sets) => {
         res.json(sets);
     });
